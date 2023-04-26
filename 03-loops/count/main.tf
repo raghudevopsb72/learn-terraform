6b66d8bbf42d0e6b5b6507be300cee6195880a20
Raghu K <raghuk.vit@gmail.com>
2023-04-26 07:22:00 +0530

    Terraform

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -2,3 +2,11 @@ resource "null_resource" "null" {
   count = 10
 }
 
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+}
+
+variable "fruits" {
+  default = [ "apple", "banana", "orange"]
+}
+
