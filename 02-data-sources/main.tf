45afb91a1028730cb9859d832c2444c37dd82d7e
Raghu K <raghuk.vit@gmail.com>
2023-04-26 07:09:03 +0530

    Terraform

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -8,7 +8,16 @@
 
 data "aws_security_groups" "test" {}
 
+data "aws_security_group" "single" {
+  count = length(data.aws_security_groups.test.ids)
+  id = data.aws_security_groups.test.ids[count.index]
+}
+
 output "all_sg" {
   value = data.aws_security_groups.test
 }
 
+
+output "single" {
+  value = data.aws_security_group.single
+}
