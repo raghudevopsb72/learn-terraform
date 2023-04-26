c39b6b722d28291253172d3d177d251583e15bd0
Raghu K <raghuk.vit@gmail.com>
2023-04-26 07:05:45 +0530

    Terraform

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -1,8 +1,14 @@
-data "aws_security_group" "selected" {
-  name = "allow-all1"
-}
+//data "aws_security_group" "selected" {
+//  name = "allow-all1"
+//}
 
-output "security_group_id" {
-  value = coalesce(data.aws_security_group.selected.id, "")
+//output "security_group_id" {
+//  value = coalesce(data.aws_security_group.selected.id, "")
+//}
+
+data "aws_security_groups" "test" {}
+
+output "all_sg" {
+  value = data.aws_security_groups.test
 }
 
