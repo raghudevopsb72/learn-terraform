b009a3529c40afe421aa9cc17b4d804e4852489b
Raghu K <raghuk.vit@gmail.com>
2023-05-02 06:32:52 +0530

    Terraform

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -3,26 +3,33 @@ resource "null_resource" "fruits" {
   for_each = var.fruits
 
   provisioner "local-exec" {
-    command = "echo Fruit Name -  ${var.fruits[each.value["name"]]}"
+    command = "echo Fruit Name - ${each.key}"
     //command = "echo  ${length(var.fruits)}"
   }
 
 }
 
+//variable "fruits" {
+//  default = {
+//    apple = {
+//      name = "apple"
+//      count = 10
+//    }
+//    orange = {
+//      name = "orange"
+//      count = 200
+//    }
+//    banana = {
+//      name = "banana"
+//      count = 100
+//    }
+//  }
+//}
+
 variable "fruits" {
   default = {
-    apple = {
-      name = "apple"
-      count = 10
-    }
-    orange = {
-      name = "orange"
-      count = 200
-    }
-    banana = {
-      name = "banana"
-      count = 100
-    }
+    apple   = 10
+    banana  = 100
+    oranges = 200
   }
 }
-
