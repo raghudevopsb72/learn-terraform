0f17218835cb77bebf0f1b490e8da7b2786cbdc0
Raghu K <raghuk.vit@gmail.com>
2023-05-02 06:23:25 +0530

    Terraform

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -1,9 +1,9 @@
 resource "null_resource" "fruits" {
-  //count = length(var.fruits)
+  count = length(var.fruits)
 
   provisioner "local-exec" {
-    //command = "echo Fruit Name -  ${var.fruits[count.index]}"
-    command = "echo  ${length(var.fruits)}"
+    command = "echo Fruit Name -  ${var.fruits[count.index]}"
+    //command = "echo  ${length(var.fruits)}"
   }
 
 }
