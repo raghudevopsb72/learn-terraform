b78feceae1022aaf62ecbebc41a579e12b1ec55b
Raghu K <raghuk.vit@gmail.com>
2023-05-03 07:15:35 +0530

    Update learn-terraform/

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -11,9 +11,19 @@ variable "sample1" {
   default = ["abc", "xyz"]
 }
 
+variable "sample2" {
+  default = {
+    xyz = 100
+    abc = 200
+  }
+}
+
 ## Print first value like var.sample1[0] with a function
 
 output "sample1" {
   value = element(var.sample1, 0)
 }
 
+output "sample2" {
+  value = var.sample2["a1"]
+}
