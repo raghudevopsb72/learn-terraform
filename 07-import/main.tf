9f791e0762dfb7de55f8a4da462abbc1bba7583e
Raghu K <raghuk.vit@gmail.com>
2023-05-04 07:32:01 +0530

    Update learn-terraform/

--- 07-import/main.tf
+++ 07-import/main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "workstation" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.small"
+  tags = {
+    "Name" = "workstation"
+  }
 }
 
 output "private_ip" {
